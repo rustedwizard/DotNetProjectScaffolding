808d32e9805df016c1b13587482470c78e57d756
Rusted Wizard <chensike89@outlook.com>
2021-01-15 16:49:08 -0800

    Added new solution function

--- dotnet-project-scaffolding/dotnet-project-scaffolding.psd1
+++ dotnet-project-scaffolding/dotnet-project-scaffolding.psd1
@@ -69,7 +69,7 @@ Description = 'Simple PowerShell module used to scaffolding a dotnet core projec
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = @("New-dotNetProject")
+FunctionsToExport = @("New-dotNetProject", "New-dotNetSolution")
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = @()
--- dotnet-project-scaffolding/dotnet-project-scaffolding.psm1
+++ dotnet-project-scaffolding/dotnet-project-scaffolding.psm1
@@ -1,3 +1,26 @@
+function Open_In_Code{
+    #if Visual Studio code is not found, then abort!
+    if ($null -eq (Get-Command "code.cmd" -ErrorAction Stop)) {
+        Write-Host "Scafolding is complete, but Visual Studio Code is not detected on your computer, so it will not be started."
+    }
+    code . -n
+}
+
+function Check_DotNet{
+    #If dotnet tool is not found, then abort!
+    if ($null -eq (Get-Command "dotnet.exe" -ErrorAction Stop)) {
+        Write-Host "It seems dotnet is not installed or in your PATH"
+    }
+}
+
+function Check-Git{
+    #if git is not found stop initializing repo, call Open in code function
+    if ($null -eq (Get-Command "git.exe" -ErrorAction Stop)) {
+        Write-Host "Scafolding is done, but Git could not be found on this machine, stop adding the project to Version Control"
+        Write-Host "Attempting to open project in Visual Studio Code..."
+        Open_In_Code
+    }
+}
 function New-dotNetProject {
     param(
         [Parameter(Mandatory = $true)]
@@ -8,21 +31,9 @@ function New-dotNetProject {
         $Language = 'C#'
     )
 
-    #Open in visual studio code
-    function Open_In_Code {
-        #if Visual Studio code is not found, then abort!
-        if ($null -eq (Get-Command "code.cmd" -ErrorAction Stop)) {
-            Write-Host "Project scafolding is complete, but Visual Studio Code is not detected on your computer, so it will not be started."
-        }
-        code . -n
-    }
-
     #Creating new project
     Write-Host "Creatint new dotnet project..."
-    #If dotnet tool is not found, then abort!
-    if ($null -eq (Get-Command "dotnet.exe" -ErrorAction Stop)) {
-        Write-Host "It seems dotnet is not installed or in your PATH"
-    }
+    $null = Check_DotNet
     dotnet new $Project_Type --language $Language --output $Project_Name
 
     $currentLocation = Get-Location;
@@ -30,12 +41,7 @@ function New-dotNetProject {
 
     #Put the project under version control
     Write-Host "Initializing version control..."
-    #if git is not found stop initializing repo, call Open in code function
-    if ($null -eq (Get-Command "git.exe" -ErrorAction Stop)) {
-        Write-Host "Project scafolding is done, but Git could not be found on this machine, stop adding the project to Version Control"
-        Write-Host "Attempting to open project in Visual Studio Code..."
-        Open_In_Code
-    }
+    $null = Check-Git
     $null = Copy-Item -Path $PSScriptRoot/gitignore -Destination $currentLocation/$Project_Name
     $null = Rename-Item $currentLocation/$Project_Name/gitignore -NewName .gitignore
     $null = git init
@@ -45,9 +51,31 @@ function New-dotNetProject {
 
     #call open in code function
     Write-Host "Attempting to open project in Visual Studio Code..."
-    Open_In_Code
+    $null = Open_In_Code
 
-    Write-Host "Project scafolding Complete!"
+    Write-Host "Project scaffolding Complete!"
 }
-Export-ModuleMember -Function New-dotNetProject
+
+function New-dotNetSolution{
+    #Create new Solution
+    $null = Check_DotNet
+    dotnet new sln
+
+    $currentLocation = Get-Location
+    #Attempt to add to Version Control
+    Write-Host "Initializing Version Control..."
+    $null = Check-Git
+    $null = Copy-Item -Path $PSScriptRoot/gitignore -Destination $currentLocation
+    $null = Rename-Item $currentLocation/gitignore -NewName .gitignore
+    $null = git init
+    $null = git add . 
+    Write-Host "Commit for the first time..."
+    $null = git commit -m "Initial Solution Creation"
+
+    #Attempting to open in visual studio code
+    Write-Host "Attempting to open project in Visual Studio Code..."
+    $null = Open_In_Code
+}
+
+Export-ModuleMember -Function New-dotNetProject, New-dotNetSolution
 
